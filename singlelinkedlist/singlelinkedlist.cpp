c4ece21ecf2d10834ada2b14cb1829bd0d8dc50e
zowiedrid <102651464+zowiedrid@users.noreply.github.com>
2024-04-25 18:20:34 +0700

    membuat perulangan node nim

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -33,6 +33,21 @@ void addNode() {
         START = nodeBaru;
         return;
     }
+
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim > current->noMhs)) {
+        if (nim == current->noMhs) {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
 }
 
 int main()
