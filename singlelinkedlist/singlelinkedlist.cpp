aaa73fcedb558b88c8c024dfcfde1a329568f571
zowiedrid <102651464+zowiedrid@users.noreply.github.com>
2024-04-25 18:22:37 +0700

    membuat void searchnode

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -50,6 +50,17 @@ void addNode() {
     previous->next = nodeBaru;
 }
 
+bool searchNode(int nim, Node*& previous, Node*& current) {
+    previous = START;
+    current = START;
+
+    while (current != NULL && nim != current->noMhs) {
+        previous = current;
+        current = current->next;
+    }
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
