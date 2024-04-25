4cf2e9795b868ecaed07bcecf34aad96d52951cb
zowiedrid <102651464+zowiedrid@users.noreply.github.com>
2024-04-25 18:17:18 +0700

    menambahkan node baru untuk NIM

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -22,6 +22,17 @@ void addNode() {
     cin >> nama;
     nodeBaru->noMhs = nim;
     nodeBaru->name = nama;
+
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs) {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
 }
 
 int main()
